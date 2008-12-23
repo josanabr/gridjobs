c519cf67ff4b91e618e36aa7915f76cedd8ac628
John A. Sanabria <john.sanabria@ece.uprm.edu>
2008-12-23 10:45:32 -0400

    Adding debuggin message

--- grails-app/services/MonitorService.groovy
+++ grails-app/services/MonitorService.groovy
@@ -74,6 +74,8 @@ class MonitorService {
       // I got the most recent
       def rc = results.first()
 
+      println "[DEBUGGIIIIIIIINNNNNNNNNNNNGGGGGGGGG] ${rc.numnodes}"
+
       if ( (rc.numnodes != numnodes) ||  (rc.cpupernode != cpupernode) || (rc.cpuspeed != cpuspeed) ) {
          rc.numnodes = numnodes
          rc.cpupernode = cpupernode 
