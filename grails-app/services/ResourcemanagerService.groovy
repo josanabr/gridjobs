f8c33f359a41cecda5503ef3bfa8c0c6735667ae
John A. Sanabria <john.sanabria@ece.uprm.edu>
2009-01-06 14:54:31 -0400

    The 'def rc = ...' was moved inside the 'try { } catch { }' block

--- grails-app/services/ResourcemanagerService.groovy
+++ grails-app/services/ResourcemanagerService.groovy
@@ -60,12 +60,12 @@ class ResourcemanagerService {
        def counter = 0
        // http://graemerocher.blogspot.com/2008/10/new-gorm-features-coming-in-11.html
        while (flag) {
-          def rc = Resourcecharacteristics.findByGridresource(gr, [lock: true])
           flag = false
-          if ( (rc.numnodes - (rc.dead + rc.inuse)) > 0) {
-             rc.inuse++
              // http://grails.org/doc/1.0.x/guide/single.html#5.3.5 Pessimistic and Optimistic Locking
-             try { 
+          try { 
+             def rc = Resourcecharacteristics.findByGridresource(gr, [lock: true])
+             if ( (rc.numnodes - (rc.dead + rc.inuse)) > 0) {
+                rc.inuse++
                 if (rc.save(flush: true) == null) {
                    println "[ResourceManagerService - allocatenode (${util.joda.Util.datetime()})] Error updating the 'rc' record"
                    rc.errors.allErrors.each {
@@ -75,22 +75,22 @@ class ResourcemanagerService {
                 }
                 println "[ResourceManagerService - allocatenode (${util.joda.Util.datetime()})] record updated!"
                 return 0 // allocated and saved
-             } catch (Exception e) {
-                println "[ResourceManagerService - allocatenode (${util.joda.Util.datetime()})] Exception updating the 'rc' record"
-                println "[ResourceManagerService - allocatenode] ${e}"
-                flag = true
-                def randomwaitingtime = util.Util.random(low,high)
-                println "[ResourceManagerService - allocatenode] \t Waiting for ${randomwaitingtime} ms"
-                Thread.sleep(randomwaitingtime)
-                counter++
+             } else  {
+                println "[ResourceManagerService - allocatenode (${util.joda.Util.datetime()})] No nodes available!"
+                return 0 // no nodes available
              }
-             if (counter > maxtries) {
-                println "[ResourceManagerService - allocatenode (${util.joda.Util.datetime()})] Maximum number of tries reached"
-                return -1 // maximum number of tries reached
-             }
-          } else  {
-             println "[ResourceManagerService - allocatenode (${util.joda.Util.datetime()})] No nodes available!"
-             return 0 // no nodes available
+          } catch (Exception e) {
+             println "[ResourceManagerService - allocatenode (${util.joda.Util.datetime()})] Exception updating the 'rc' record"
+             println "[ResourceManagerService - allocatenode] ${e}"
+             flag = true
+             def randomwaitingtime = util.Util.random(low,high)
+             println "[ResourceManagerService - allocatenode] \t Waiting for ${randomwaitingtime} ms"
+             Thread.sleep(randomwaitingtime)
+             counter++
+          }
+          if (counter > maxtries) {
+             println "[ResourceManagerService - allocatenode (${util.joda.Util.datetime()})] Maximum number of tries reached"
+             return -1 // maximum number of tries reached
           }
        }
        println "[ResourceManagerService - allocatenode (${util.joda.Util.datetime()})] Exiting by default :-|"
@@ -126,13 +126,13 @@ class ResourcemanagerService {
        def counter = 0
        while (flag) {
           flag = false
-          def rc = Resourcecharacteristics.findByGridresource(gr, [lock: true])
-          if (rc.inuse <= 0) {
-             println "[ResourceManagerService - releasenode (${util.joda.Util.datetime()})] inuse less or equal to zero :-|"
-             return -1
-          }
-          rc.inuse--
           try { 
+             def rc = Resourcecharacteristics.findByGridresource(gr, [lock: true])
+             if (rc.inuse <= 0) {
+                println "[ResourceManagerService - releasenode (${util.joda.Util.datetime()})] inuse less or equal to zero :-|"
+                return -1
+             }
+             rc.inuse--
              if (rc.save(flush: true) == null) {
                 println "[ResourceManagerService - releasenode (${util.joda.Util.datetime()})] Error updating the 'rc' record"
                 rc.errors.allErrors.each {
